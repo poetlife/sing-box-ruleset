3509b394a138c3bd6b9d1b41efed39fed2f451a9
Pykt <pengjidong04@Gmail.com>
2024-01-28 16:51:21 +0800

    init rulesets

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sing-box-ruleset
 including singbox ruleset
+
+原本的sing-box规则只能支持json配置，但是json配置起来太麻烦了，而且不支持注释，因此本repo收集一些规则集，并使用yaml进行标注，再转换成json配置的规则集，包含的规则如下：
+- `mainland-only` 该部分规则集包含仅能再中国大陆访问的站点
+
+
+##  compile-related
+
+yaml to json
+```bash
+# 将rulesets文件中所有的yaml格式转换为json格式
+python convert.py
+```
+
+compile rule to binary
+```bash
+sing-box rule-set compile config.json
+```
---
+++ convert.py
@@ -0,0 +1,34 @@
+import yaml
+import json
+import os
+
+def convert_yaml_to_json(yaml_file, json_file):
+    with open(yaml_file, 'r', encoding="utf-8") as yaml_stream:
+        # 解析 YAML 文件
+        yaml_data = yaml.safe_load(yaml_stream)
+
+    # 将 Python 对象转换为 JSON 格式
+    json_data = json.dumps(yaml_data, indent=2)
+
+    # 将 JSON 数据写入文件
+    with open(json_file, 'w', encoding="utf-8") as json_stream:
+        json_stream.write(json_data)
+
+def mkdir(name):
+    if os.path.exists(name):
+        return 
+    else:
+        os.mkdir(name)
+
+
+if __name__ == "__main__":
+    """将rulesets中所有yaml的文件都转换为json格式存入到sources中去"""
+    mkdir("sources")
+    mkdir("output")
+    for root, dirs, files in os.walk("rulesets"):
+        for file in files:
+            path = os.path.join(root, file)
+            _, extension = os.path.splitext(path)
+            # print(filename, extension)
+            if extension in [".yml", ".yaml"]:
+                convert_yaml_to_json(path, f"sources/{file}.json")
